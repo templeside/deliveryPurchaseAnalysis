cf66367083fd0e60b80d6e1f4f3ba9bc54c369fc
Chanwoong Jhon <cwjhon5654@gmail.com>
2022-04-01 11:53:52 +0000

    updating donate a/btesting

--- main.py
+++ main.py
@@ -26,11 +26,34 @@ def home():
 
 @app.route('/browse.html')
 def browse():
-    return df.to_html()
+#     fonts: https://www.w3schools.com/cssref/tryit.asp?filename=trycss_font_timesnewroman
+    df_html = df.to_html()
+    return "<html>{}<html>".format("<h1>Browse</h1>"+df_html) 
+
+#When setting up your A/B test for the donate page, consider creating an <a> tag on your index.html. 
+#In that link, you could set the href to already include one of the queries (A or B). 
+#After that, you need to figure out how to swap this query back and forth when the index.html is revisited. 
+#A good way to do this may be to do string manipulation on the html you read in (such as using .replace()). 
+
+# donate_visited_counter = 0
 
 @app.route('/donate.html')
-def donate():
-    return "donate or die..."
+def donate():    
+#     donate_visited_counter +=1
+#         if(donate_visited_counter<=10):
+#             if(donate_visited_counter%2 ==0):
+#                 # do something with A style
+#             else:
+#                 # do something with B style
+#         else:
+#             if(aCTR> bCTR):
+#                 # do something with A style
+#             else:
+#                 # do something with B style
+
+    with open("donate.html") as f:
+        html = f.read()
+    return html
 
 @app.route('/email', methods=["POST"])
 def email():
