39749b25826cc6ff12e45a6d01e62d158905ec98
Chanwoong Jhon <cwjhon5654@gmail.com>
2022-04-02 00:37:04 +0000

    updated and got 35% /100%

--- main.py
+++ main.py
@@ -17,11 +17,55 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 df = pd.read_csv("main.csv")
-# print(df)
+
+
+clickCounter = {
+    "count": 0,
+    "styleA":0,
+    "styleB": 0,
+    "clickedA":0,
+    "clickedB":0,
+    "CtrA": 0, 
+    "CtrB":0
+}
+
+clickCounter["count"] = 0
+
 @app.route('/')
 def home():
+    global clickCounter
+#     print(clickCounter)
     with open("index.html") as f:
         html = f.read()
+    clickCounter["count"] = clickCounter["count"]+1
+#     print("current count is "+ str(clickCounter["count"]))
+    
+    if(clickCounter["count"]<=10):
+        if(clickCounter["count"]%2 ==0):
+            # do something with A style - red
+            html = html.replace('<a href="./donate.html" target="_blank">Donate</a>',
+                                '<a href="./donate.html?from=A" target="_blank" style="color:red;"><b>Donate</b></a>' )
+            clickCounter['styleA'] +=1
+        else:
+            # do something with B style - blue
+            html = html.replace('<a href="./donate.html" target="_blank">Donate</a>',
+                                '<a href="./donate.html?from=B" target="_blank" style="color:blue;"><b>Donate</b></a>' )
+            clickCounter['styleB'] +=1
+    #after 10 counts
+    else:
+        if(clickCounter['CtrA']>= clickCounter['CtrB']):
+#             # do something with A style
+            html = html.replace('<a href="./donate.html" target="_blank">Donate</a>',
+                                '<a href="./donate.html?from=A" target="_blank" style="color:red;"><b>Donate</b></a>' )
+            clickCounter['styleA'] +=1
+
+        else:
+#             # do something with B style
+            html = html.replace('<a href="./donate.html" target="_blank">Donate</a>',
+                                '<a href="./donate.html?from=B" target="_blank" style="color:blue;"><b>Donate</b></a>' )
+            clickCounter['styleB'] +=1
+
+    
     return html
 
 @app.route('/browse.html')
@@ -30,29 +74,35 @@ def browse():
     df_html = df.to_html()
     return "<html>{}<html>".format("<h1>Browse</h1>"+df_html) 
 
-#When setting up your A/B test for the donate page, consider creating an <a> tag on your index.html. 
-#In that link, you could set the href to already include one of the queries (A or B). 
-#After that, you need to figure out how to swap this query back and forth when the index.html is revisited. 
-#A good way to do this may be to do string manipulation on the html you read in (such as using .replace()). 
-
-# donate_visited_counter = 0
-
+# clickCounter = {
+#     "count": 0,
+#     "styleA":0,
+#     "styleB": 0,
+#     "clickedA":0,
+#     "clickedB":0,
+#     "CtrA": 0, 
+#     "CtrB":0
+# }
 @app.route('/donate.html')
 def donate():    
-#     donate_visited_counter +=1
-#         if(donate_visited_counter<=10):
-#             if(donate_visited_counter%2 ==0):
-#                 # do something with A style
-#             else:
-#                 # do something with B style
-#         else:
-#             if(aCTR> bCTR):
-#                 # do something with A style
-#             else:
-#                 # do something with B style
+    global clickCounter
 
     with open("donate.html") as f:
         html = f.read()
+        
+    args = dict(request.args)
+#     print(args['from'])
+#     print(type(args['from']))
+#     print(clickCounter['styleA'])
+#     print(clickCounter['styleB'])
+    if(args['from'] == 'A'):
+        clickCounter['clickedA'] +=1
+        clickCounter['CtrA'] = clickCounter['clickedA']/ clickCounter['styleA']
+    elif(args['from'] == 'B'):
+        clickCounter['clickedB'] +=1
+        clickCounter['CtrB'] = clickCounter['clickedB']/ clickCounter['styleB']
+    
+#     print('clicked is', args['from'])
     return html
 
 @app.route('/email', methods=["POST"])
